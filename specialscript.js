e993f1c4279915cb844d5de6426a158a6d368719
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-03-27 11:36:34 +0300

    Add files via upload

---
+++ specialscript.js
@@ -0,0 +1,11 @@
+let loader = document.getElementById("loader");
+
+
+
+window.onload = function() {
+    // Get a reference to the loader element
+    var loader = document.getElementById("loader");
+  
+    // Hide the loader
+    loader.style.display = "none";
+}
